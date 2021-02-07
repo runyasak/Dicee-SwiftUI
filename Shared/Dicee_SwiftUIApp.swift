c68141c7504eeb06ec337bae6b37454d45f9c00f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-07 19:35:34 +0700

    Initial Commit

---
+++ Shared/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Runyasak Chaengnaimuang on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
